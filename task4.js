f64b3ef9746e47af610baccd3506f4cf56ddc180
ornobaadi <aadi4789@gmail.com>
2024-09-07 22:50:38 +0600

    Completed

---
+++ task4.js
@@ -0,0 +1,12 @@
+let flowers = ['Rose','Lily', 'Sunflower'];
+let num = [1,2,3,4,5];
+
+let blank = [];
+let food = 'BBQ Chicken Burger';
+let year = 2025;
+
+console.log(Array.isArray(flowers));
+console.log(Array.isArray(num));
+console.log(Array.isArray(blank));
+console.log(Array.isArray(food));
+console.log(Array.isArray(year));
---
+++ task5.js
@@ -0,0 +1,8 @@
+let num1 = [10, 15, 20, 25, 30];
+let num2 = ['1', '2', '4', '4', '5'];
+
+let combinedOutput = num1.concat(num2);
+
+console.log(num1);
+console.log(num2);
+console.log(combinedOutput);
